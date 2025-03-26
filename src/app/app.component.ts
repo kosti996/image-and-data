87c9c193e763e99d4ce3c69876b7526d6525f588
kosti996 <kostiukevycha@gmail.com>
2025-03-26 15:28:57 +0100

    added upload form & changed readme file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UploadComponent } from "./upload/upload.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [UploadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-upload',
-  imports: [],
   templateUrl: './upload.component.html',
-  styleUrl: './upload.component.css'
+  imports: [ReactiveFormsModule]
 })
 export class UploadComponent {
+  uploadForm: FormGroup;
+  selectedFile: File | null = null;
 
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.uploadForm = this.fb.group({
+      dropdown: [''],
+      textInput: [''],
+      freeText: ['']
+    });
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('dropdown', this.uploadForm.get('dropdown')?.value);
+    formData.append('textInput', this.uploadForm.get('textInput')?.value);
+    formData.append('freeText', this.uploadForm.get('freeText')?.value);
+    if (this.selectedFile) {
+      formData.append('file', this.selectedFile, this.selectedFile.name);
+    }
+
+    this.http.post('https://localhost:7122/Upload', formData).subscribe(
+      (response) => console.log('Success!', response),
+      (error) => console.error('Error!', error)
+    );
+  }
 }
